ecbe2cbaf7946b2772065fbbd6456b56256b261d
Kalle Huttunen <khuttun@gmail.com>
2020-03-14 23:10:07 +0200

    Initial game physics

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,8 @@
+use std::time::Instant;
+
 pub const BALL_R: f32 = 1.0;
 pub const WALL_H: f32 = 1.5;
+const ACCEL_COEFF: f32 = 100.0 / std::f32::consts::PI;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Point {
@@ -46,6 +49,8 @@ impl From<&json::JsonValue> for Rect {
     }
 }
 
+// Level coordinate system origin is in top-left corner of the board.
+// Positive x-axis direction is right and positive y-axis direction down.
 pub struct Level {
     pub name: String,
     pub size: Size,
@@ -70,22 +75,75 @@ impl Level {
 }
 
 #[derive(Copy, Clone, Debug)]
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Copy, Clone, Debug)]
 pub enum State {
-    InProgress { ball_pos: Point },
+    InProgress {
+        ball_pos: Point,
+        ball_v: Velocity,
+    },
     Won,
     Lost,
 }
 
 pub struct Game {
     pub state: State,
+    pub prev_update: Option<Instant>,
+    pub angle_x: f32, // Board angle w.r.t. x-axis in radians
+    pub angle_y: f32,
     pub level: Level,
 }
 
 impl Game {
     pub fn new(lvl: Level) -> Game {
         Game {
-            state: State::InProgress{ ball_pos: lvl.start },
+            state: State::InProgress {
+                ball_pos: lvl.start,
+                ball_v: Velocity { x: 0.0, y: 0.0},
+            },
+            prev_update: None,
+            angle_x: 0.0,
+            angle_y: 0.0,
             level: lvl,
+        }
+    }
+
+    pub fn set_x_angle(&mut self, angle: f32) {
+        self.angle_x = angle;
+    }
+
+    pub fn set_y_angle(&mut self, angle: f32) {
+        self.angle_y = angle;
+    }
+
+    pub fn update(&mut self, time: Instant) {
+        match self.state {
+            State::InProgress { ball_pos, ball_v } => {
+                match self.prev_update {
+                    None => {
+                        self.prev_update = Some(time);
+                    },
+                    Some(prev_time) => {
+                        let dt = time.duration_since(prev_time).as_secs_f32();
+                        let a_x = self.angle_x * ACCEL_COEFF;
+                        let a_y = self.angle_y * ACCEL_COEFF;
+                        let v_x = ball_v.x + a_x * dt;
+                        let v_y = ball_v.y + a_y * dt;
+                        let pos_x = ball_pos.x + v_x * dt;
+                        let pos_y = ball_pos.y + v_y * dt;
+                        self.state = State::InProgress {
+                            ball_pos: Point { x: pos_x, y: pos_y },
+                            ball_v: Velocity { x: v_x, y: v_y },
+                        };
+                        self.prev_update = Some(time);
+                    },
+                }
+            },
+            _ => (),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate glium;
 use glium::glutin;
+use glutin::event::{Event, StartCause, WindowEvent};
 use std::rc::Rc;
+use std::time::{Duration, Instant};
 mod game;
 mod graphics;
 
@@ -11,14 +13,18 @@ fn main() {
     let cb = glutin::ContextBuilder::new().with_depth_buffer(24).with_multisampling(2);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let monitor = display.gl_window().window().available_monitors().next().unwrap();
-    let w = monitor.size().width;
-    let h = monitor.size().height;
-    println!("Monitor size {} x {}", w, h);
-    display.gl_window().window().set_fullscreen(Some(glutin::window::Fullscreen::Borderless(monitor)));
+    let w = display.gl_window().window().inner_size().width as f32;
+    let h = display.gl_window().window().inner_size().height as f32;
+
+    // let monitor = display.gl_window().window().available_monitors().next().unwrap();
+    // let w = monitor.size().width;
+    // let h = monitor.size().height;
+    // display.gl_window().window().set_fullscreen(Some(glutin::window::Fullscreen::Borderless(monitor)));
+
+    println!("Window size {} x {}", w, h);
 
     display.gl_window().window().set_cursor_position(
-        glutin::dpi::PhysicalPosition::new(w / 2, h / 2)).unwrap();
+        glutin::dpi::PhysicalPosition::new(w / 2.0, h / 2.0)).unwrap();
     display.gl_window().window().set_cursor_visible(false);
 
     let quad = Rc::new(graphics::Shape::from_ply(&display, "quad.ply"));
@@ -28,7 +34,7 @@ fn main() {
     // Create a level and set up the scene based on it
     let level1 = game::Level::from_json("level1.json");
 
-    let mut scene = graphics::Scene::new(&display, w as f32 / h as f32);
+    let mut scene = graphics::Scene::new(&display, w / h);
 
     let mut board = graphics::Object::new(&quad);
     board.set_color(0.75, 0.55, 0.3);
@@ -57,25 +63,29 @@ fn main() {
         level1.start.x, 0.0, level1.start.y);
 
     // Create a new game from the level and enter the main event loop
-    let game = game::Game::new(level1);
+    let mut game = game::Game::new(level1);
 
     event_loop.run(move |event, _, control_flow| {
-        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        // Wake up after a deadline if no other events are received
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(Instant::now() + Duration::from_nanos(16_666_667));
 
         match event {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
                 },
-                // glutin::event::WindowEvent::CursorMoved { position, .. } => {
-                //     scene.get_object(ball_id).set_position(
-                //         5.0f32 * ((position.x as f32) - (w as f32) / 2.0f32) / ((w as f32) / 2.0f32),
-                //         0.0f32,
-                //         5.0f32 * ((position.y as f32) - (h as f32) / 2.0f32) / ((h as f32) / 2.0f32))
-                // },
-                glutin::event::WindowEvent::KeyboardInput {
+                WindowEvent::CursorMoved { position, .. } => {
+                    let angle_x = (position.x as f32 - w / 2.0) / (w / 2.0) * (std::f32::consts::PI / 8.0);
+                    let angle_y = (position.y as f32 - h / 2.0) / (h / 2.0) * (std::f32::consts::PI / 8.0);
+                    game.set_x_angle(angle_x);
+                    game.set_y_angle(angle_y);
+                    println!("Board angle ({}°, {}°)",
+                        angle_x * 180.0 / std::f32::consts::PI,
+                        angle_y * 180.0 / std::f32::consts::PI);
+                    // -> proceed to update game state and draw
+                },
+                WindowEvent::KeyboardInput {
                     input: glutin::event::KeyboardInput {
                         virtual_keycode: Some(glutin::event::VirtualKeyCode::Escape),
                         state: glutin::event::ElementState::Pressed,
@@ -85,22 +95,27 @@ fn main() {
                 } => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
-                }
+                },
                 _ => return,
             },
-            glutin::event::Event::NewEvents(cause) => match cause {
-                glutin::event::StartCause::ResumeTimeReached { .. } => {
-                    match game.state {
-                        game::State::InProgress { ball_pos } => {
-                            scene.get_object(ball_id).set_position(ball_pos.x, game::BALL_R, ball_pos.y);
-                        }
-                        _ => ()
-                    }
-                },
-                glutin::event::StartCause::Init => (),
+            Event::NewEvents(cause) => match cause {
+                // woke up after deadline -> proceed to update game state and draw
+                StartCause::ResumeTimeReached { .. } => (), 
                 _ => return,
             },
             _ => return,
+        }
+
+        game.update(Instant::now());
+
+        match game.state {
+            game::State::InProgress { ball_pos, .. } => {
+                scene.get_object(ball_id).set_position(ball_pos.x, game::BALL_R, ball_pos.y);
+                scene.look_at(
+                    ball_pos.x, 30.0 * game::BALL_R, ball_pos.y + 30.0 * game::BALL_R,
+                    ball_pos.x, 0.0, ball_pos.y);
+            },
+            _ => (),
         }
 
         let mut target = display.draw();
