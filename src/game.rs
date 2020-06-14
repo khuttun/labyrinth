e7ced1f84c0bffdf13c9203175bc83cebb57fd05
Kalle Huttunen <khuttun@gmail.com>
2020-06-14 22:02:08 +0300

    Use glm in game

--- src/game.rs
+++ src/game.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 pub const BALL_R: f32 = 20.0;
 pub const HOLE_R: f32 = 1.2 * BALL_R;
 pub const MAX_ANGLE: f32 = PI / 32.0;
-const ACCEL_COEFF: f32 = 300.0 * BALL_R / std::f32::consts::PI;
+const ACCEL_COEFF: f32 = 300.0 * BALL_R / PI;
 const BOUNCE_COEFF: f32 = 0.2;
 
 #[derive(Copy, Clone, Debug)]
@@ -23,13 +23,33 @@ impl From<&json::JsonValue> for Point {
     }
 }
 
-impl Point {
-    pub fn distance_to(&self, other: &Point) -> f32 {
-        ((other.x - self.x).powi(2) + (other.y - self.y).powi(2)).sqrt()
+impl From<&glm::Vec2> for Point {
+    fn from(v: &glm::Vec2) -> Point {
+        Point { x: v.x, y: v.y }
     }
+}
 
-    pub fn angle_to(&self, other: &Point) -> f32 {
-        (other.y - self.y).atan2(other.x - self.x)
+impl From<&Point> for glm::Vec2 {
+    fn from(p: &Point) -> glm::Vec2 {
+        glm::vec2(p.x, p.y)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl From<&glm::Vec2> for Velocity {
+    fn from(v: &glm::Vec2) -> Velocity {
+        Velocity { x: v.x, y: v.y }
+    }
+}
+
+impl From<&Velocity> for glm::Vec2 {
+    fn from(v: &Velocity) -> glm::Vec2 {
+        glm::vec2(v.x, v.y)
     }
 }
 
@@ -65,7 +85,7 @@ impl From<&json::JsonValue> for Rect {
 
 impl Rect {
     // Check if self contains Point p
-    fn contains(&self, p: &Point) -> bool {
+    fn contains(&self, p: glm::Vec2) -> bool {
         p.x >= self.pos.x && p.x < self.pos.x + self.size.w && p.y >= self.pos.y && p.y < self.pos.y + self.size.h
     }
 }
@@ -94,12 +114,6 @@ impl Level {
             holes: data["holes"].members().map(|j| Point::from(j)).collect(),
         }
     }
-}
-
-#[derive(Copy, Clone, Debug)]
-pub struct Velocity {
-    pub x: f32,
-    pub y: f32,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -136,11 +150,11 @@ impl Game {
     }
 
     pub fn rotate_x(&mut self, angle: f32) {
-        self.angle_x = clamp(self.angle_x + angle, -MAX_ANGLE, MAX_ANGLE);
+        self.angle_x = glm::clamp_scalar(self.angle_x + angle, -MAX_ANGLE, MAX_ANGLE);
     }
 
     pub fn rotate_y(&mut self, angle: f32) {
-        self.angle_y = clamp(self.angle_y + angle, -MAX_ANGLE, MAX_ANGLE);
+        self.angle_y = glm::clamp_scalar(self.angle_y + angle, -MAX_ANGLE, MAX_ANGLE);
     }
 
     pub fn update(&mut self, time: Instant) {
@@ -151,136 +165,97 @@ impl Game {
 
         let (p, v) = self.detect_collisions(self.do_physics(time));
         
-        if self.level.end.contains(&p) {
+        if self.level.end.contains(p) {
             self.state = State::Won;
         }
 
-        let hole = self.level.holes.iter().find(|h| p.distance_to(h) < HOLE_R);
+        let hole = self.level.holes.iter().find(|h| glm::distance(&p, &glm::Vec2::from(*h)) < HOLE_R);
         if hole.is_some() {
             self.state = State::Lost { hole: *hole.unwrap(), t_lost: time };
         }
 
         self.prev_update = Some(time);
-        self.ball_pos = p;
-        self.ball_v = v;
+        self.ball_pos = Point::from(&p);
+        self.ball_v = Velocity::from(&v);
     }
 
-    fn do_physics(&self, time: Instant) -> (Point, Velocity) {
+    // Calculate ball (position, velocity) based on current time
+    fn do_physics(&self, time: Instant) -> (glm::Vec2, glm::Vec2) {
         let dt = time.duration_since(self.prev_update.unwrap_or(time)).as_secs_f32();
-
-        let v = Velocity {
-            x: self.ball_v.x + self.angle_x * ACCEL_COEFF * dt,
-            y: self.ball_v.y + self.angle_y * ACCEL_COEFF * dt,
-        };
-        let p = Point {
-            x: self.ball_pos.x + v.x * dt,
-            y: self.ball_pos.y + v.y * dt,
-        };
-
+        let v = glm::Vec2::from(&self.ball_v) + glm::vec2(self.angle_x, self.angle_y) * ACCEL_COEFF * dt;
+        let p = glm::Vec2::from(&self.ball_pos) + v * dt;
         (p, v)
     }
 
-    fn detect_collisions(&self, (mut p, mut v): (Point, Velocity)) -> (Point, Velocity) {
-        // Board edge collisions
-        if p.x < BALL_R { // left
-            bounce_x(&mut p, &mut v, BALL_R);
-        }
-        if p.x >= self.level.size.w - BALL_R { // right
-            bounce_x(&mut p, &mut v, self.level.size.w - BALL_R);
-        }
-        if p.y < BALL_R { // top
-            bounce_y(&mut p, &mut v, BALL_R);
-        }
-        if p.y >= self.level.size.h - BALL_R { // bottom
-            bounce_y(&mut p, &mut v, self.level.size.h - BALL_R);
-        }
-
-        // Wall collisions
-        for wall in self.level.walls.iter() {
-            match detect_wall_collision(&p, wall) {
-                Some(response) => {
-                    p.x = p.x + response.0.x * response.1;
-                    p.y = p.y + response.0.y * response.1;
-                    
-                    // "Damped reflection" around the collision response direction
-                    let v0 = glm::vec2(v.x, v.y);
-                    let v1 = v0 - (1.0 + BOUNCE_COEFF) * glm::dot(&v0, &response.0) * response.0;
-                    v.x = v1.x;
-                    v.y = v1.y;
-                },
-                None => (),
-            }
-        }
-
-        (p, v)
+    // Detect collisions to walls and update ball (position, velocity) accordingly
+    fn detect_collisions(&self, pv: (glm::Vec2, glm::Vec2)) -> (glm::Vec2, glm::Vec2) {
+        let pv = apply_collision_response(pv, detect_board_left_edge_collision(pv.0, self.level.size));
+        let pv = apply_collision_response(pv, detect_board_right_edge_collision(pv.0, self.level.size));
+        let pv = apply_collision_response(pv, detect_board_top_edge_collision(pv.0, self.level.size));
+        let pv = apply_collision_response(pv, detect_board_bottom_edge_collision(pv.0, self.level.size));
+        return self.level.walls.iter().fold(pv, |pv, w| apply_collision_response(pv, detect_wall_collision(pv.0, *w)));
     }
 }
 
-// Return the collision response (direction, amount) if ball collides with the wall
-fn detect_wall_collision(ball_pos: &Point, wall: &Rect) -> Option<(glm::Vec2, f32)> {
-    let wall_half_size = Size { w: wall.size.w / 2.0, h: wall.size.h / 2.0 };
-    let wall_center = Point { x: wall.pos.x + wall_half_size.w, y: wall.pos.y + wall_half_size.h };
-    let closest_point_in_wall = Point {
-        x: wall_center.x + clamp(ball_pos.x - wall_center.x, -wall_half_size.w, wall_half_size.w),
-        y: wall_center.y + clamp(ball_pos.y - wall_center.y, -wall_half_size.h, wall_half_size.h),
-    };
-    let distance_to_wall = ball_pos.distance_to(&closest_point_in_wall);
-
-    if distance_to_wall < BALL_R {
-        Some((
-            glm::normalize(&(glm::vec2(ball_pos.x, ball_pos.y) - glm::vec2(closest_point_in_wall.x, closest_point_in_wall.y))),
-            BALL_R - distance_to_wall
-        ))
+fn detect_board_left_edge_collision(ball_pos: glm::Vec2, board_size: Size) -> Option<glm::Vec2> {
+    if ball_pos.x < BALL_R {
+        Some(glm::vec2(BALL_R - ball_pos.x, 0.0))
     }
     else {
         None
     }
 }
 
-fn bounce_x(pos: &mut Point, vel: &mut Velocity, edge_x: f32) {
-    pos.x = edge_x;
-    vel.x = -BOUNCE_COEFF * vel.x;
-}
-
-fn bounce_y(pos: &mut Point, vel: &mut Velocity, edge_y: f32) {
-    pos.y = edge_y;
-    vel.y = -BOUNCE_COEFF * vel.y;
-}
-
-pub fn clamp(x: f32, min: f32, max: f32) -> f32 {
-    match x {
-        x if x < min => min,
-        x if x > max => max,
-        _ => x,
+fn detect_board_right_edge_collision(ball_pos: glm::Vec2, board_size: Size) -> Option<glm::Vec2> {
+    if ball_pos.x >= board_size.w - BALL_R {
+        Some(glm::vec2(board_size.w - BALL_R - ball_pos.x, 0.0))
+    }
+    else {
+        None
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    
-    #[derive(Copy, Clone, Debug)]
-    struct Roughly(f32);
-    impl PartialEq<f32> for Roughly {
-        fn eq(&self, other: &f32) -> bool {
-            (self.0 - other).abs() < 1e-6
-        }
+fn detect_board_top_edge_collision(ball_pos: glm::Vec2, board_size: Size) -> Option<glm::Vec2> {
+    if ball_pos.y < BALL_R {
+        Some(glm::vec2(0.0, BALL_R - ball_pos.y))
     }
-    
-    #[test]
-    fn point() {
-        assert_eq!(Roughly(0.0), Point {x: -1.0, y: 1.0}.distance_to(&Point {x: -1.0, y: 1.0}));
-        assert_eq!(Roughly(2.0), Point {x: -1.0, y: 1.0}.distance_to(&Point {x: 1.0, y: 1.0}));
-        assert_eq!(Roughly(2.0f32.sqrt()), Point {x: -1.0, y: 1.0}.distance_to(&Point {x: -2.0, y: 0.0}));
+    else {
+        None
+    }
+}
 
-        assert_eq!(Roughly(0.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 1.0, y: 1.0}));
-        assert_eq!(Roughly(0.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 2.0, y: 1.0}));
-        assert_eq!(Roughly(PI / 4.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 2.0, y: 2.0}));
-        assert_eq!(Roughly(PI / 2.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 1.0, y: 2.0}));
-        assert_eq!(Roughly(3.0 * PI / 4.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 0.0, y: 2.0}));
-        assert_eq!(Roughly(PI), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 0.0, y: 1.0}));
-        assert_eq!(Roughly(-3.0 * PI / 4.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 0.0, y: 0.0}));
-        assert_eq!(Roughly(-PI / 2.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 1.0, y: 0.0}));
-        assert_eq!(Roughly(-PI / 4.0), Point {x: 1.0, y: 1.0}.angle_to(&Point {x: 2.0, y: 0.0}));
+fn detect_board_bottom_edge_collision(ball_pos: glm::Vec2, board_size: Size) -> Option<glm::Vec2> {
+    if ball_pos.y >= board_size.h - BALL_R {
+        Some(glm::vec2(0.0, board_size.h - BALL_R - ball_pos.y))
+    }
+    else {
+        None
+    }
+}
+
+fn detect_wall_collision(ball_pos: glm::Vec2, wall: Rect) -> Option<glm::Vec2> {
+    let wall_half_size = glm::vec2(wall.size.w / 2.0, wall.size.h / 2.0);
+    let wall_center = glm::Vec2::from(&wall.pos) + wall_half_size;
+    let closest_point_in_wall = wall_center + glm::clamp_vec(&(ball_pos - wall_center), &(-wall_half_size), &wall_half_size);
+    let distance_to_wall = glm::distance(&ball_pos, &closest_point_in_wall);
+    if distance_to_wall < BALL_R {
+        Some((BALL_R - distance_to_wall) * glm::normalize(&(ball_pos - closest_point_in_wall)))
+    }
+    else {
+        None
+    }
+}
+
+fn apply_collision_response(pv: (glm::Vec2, glm::Vec2), resp: Option<glm::Vec2>) -> (glm::Vec2, glm::Vec2) {
+    match resp {
+        Some(r) => {
+            let dir = glm::normalize(&r);
+            (
+                pv.0 + r,
+                // "Damped reflection" around the collision response direction
+                pv.1 - (1.0 + BOUNCE_COEFF) * glm::dot(&pv.1, &dir) * dir,
+            )
+        },
+        None => pv,
     }
 }
