4c05e7aeb63e488fa50335daffd52619335a8b95
Kalle Huttunen <khuttun@gmail.com>
2020-10-24 22:56:30 +0300

    Use linear minifying filter for textures
    This makes the hole edges smoother.

--- src/graphics.rs
+++ src/graphics.rs
@@ -564,7 +564,7 @@ impl Texture {
             address_mode_v: wgpu::AddressMode::Repeat,
             address_mode_w: wgpu::AddressMode::Repeat,
             mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
+            min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Nearest,
             ..Default::default()
         });
